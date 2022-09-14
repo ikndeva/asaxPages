248bd1798915a14dde0255cd51d66ab93ff31c06
Hiroki Arimura <arim@ist.hokudai.ac.jp>
2022-09-14 20:41:36 +0900

    from aalto14: gridboard

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,7 +1,7 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.9',
-    LANGUAGE: 'en',
+    VERSION: '0.7',
+    LANGUAGE: 'ja',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
